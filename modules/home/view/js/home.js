3bc010b0e9575fd9c3c7f4623d977daf182f588b
unknown <serhuegi@gmail.com>
2018-05-16 20:36:06 +0200

    Corregido un error por el que en el home al buscar en el autocomplete no podías ver el details de los hoteles buscados

--- modules/home/view/js/home.js
+++ modules/home/view/js/home.js
@@ -99,15 +99,82 @@ $.ajax({
             hotel.forEach(function(element) {
 							if(i<=5){
 
-              html=html+'<article>'+
-                '<a class="image"><img src="'+element.imagen+'" alt="" /></a>'+
-                '<h3>'+element.nombre+'</h3>'+
-                '<p>'+element.fecha_entrada+' - '+ element.fecha_salida+'</p>'+
-                '<p>'+element.municipio+', '+ element.provincia+', '+element.comunidad+'</p>'+
-              '</article>';}
+								html=html+'<article>'+
+				          '<a class="image"><img src="'+element.imagen+'" alt="" /></a>'+
+				          '<h3>'+element.nombre+'</h3>'+
+				          '<p>'+element.fecha_entrada+' - '+ element.fecha_salida+'</p>'+
+				          '<p>'+element.municipio+', '+ element.provincia+', '+element.comunidad+'</p>'+
+				          '<ul class="actions">'+
+				          '<li><div class="button id" id='+element.id+'>More</div></li>'+
+				          '</ul>'+
+				        '</article>';}
 							i++;
             });
             $(".posts").html(html);
+						$(".id").on("click", function(e){
+			        var id=this.getAttribute('id');
+			        dataTosend='op='+"details"+"&id="+id;
+			        $.ajax({
+			          type: "POST",
+			          url: "../home/details/",
+			          data: dataTosend,
+			          datatype :'json',
+			          success: function(data){
+									console.log(data);
+			            json = JSON.parse(data);
+									console.log(json);
+			                   var info = [];
+			                   var image = "";
+			                   json.forEach(function(element) {
+			                     image="<img src='"+ element.imagen +"' alt='done'>";
+			                     $("#image").html(image);
+			                     $("#nombre").html(element.nombre);
+			                     $("#fecha_entrada").html(element.fecha_entrada);
+			                     $("#fecha_salida").html(element.fecha_salida);
+			                     $("#municipio").html(element.municipio);
+			                     $("#provincia").html(element.provincia);
+			                     $("#comunidad").html(element.comunidad);
+			                     $("#calidad").html(element.estrellas + " estrellas");
+			                     $("#wifi").html(element.wifi);
+			                     $("#piscina").html(element.piscina);
+			                     $("#playa").html(element.playa);
+			                     $("#actividades").html(element.actividades);
+			                     $("#comida").html(element.comida);
+			                     $("#spa").html(element.spa);
+			                     $("#observaciones").html(element.observaciones);
+			                   });
+
+
+
+			                   $("#details_hotel").show();
+
+			                   $("#hotel_modal").dialog({
+			                       width: 850, //<!-- ------------- ancho de la ventana -->
+			                       height: 500, //<!--  ------------- altura de la ventana -->
+			                       //show: "scale", <!-- ----------- animación de la ventana al aparecer -->
+			                       //hide: "scale", <!-- ----------- animación al cerrar la ventana -->
+			                       resizable: "false", //<!-- ------ fija o redimensionable si ponemos este valor a "true" -->
+			                       //position: "down",<!--  ------ posicion de la ventana en la pantalla (left, top, right...) -->
+			                       modal: "true", //<!-- ------------ si esta en true bloquea el contenido de la web mientras la ventana esta activa (muy elegante) -->
+			                       buttons: {
+			                           Ok: function () {
+			                               $(this).dialog("close");
+			                           }
+			                       },
+			                       show: {
+			                           effect: "blind",
+			                           duration: 500
+			                       },
+			                       hide: {
+			                           effect: "explode",
+			                           duration: 500
+			                       }
+			                   });
+
+
+			          }
+			        });
+			      });
 						scroll = false;
 
           }
